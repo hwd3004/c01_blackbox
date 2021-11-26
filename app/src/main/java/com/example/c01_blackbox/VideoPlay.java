c34cc35c8d29d3bfa7bdd6de0423ec6dc0ab320a
hwd3004 <hwd3004@gmail.com>
2021-11-26 14:25:51 +0900

    Update VideoPlay.java

--- app/src/main/java/com/example/c01_blackbox/VideoPlay.java
+++ app/src/main/java/com/example/c01_blackbox/VideoPlay.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,43 +19,23 @@ public class VideoPlay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_play);
 
-        hideSystemUI();
-
         extras = getIntent().getExtras();
 
         if (extras != null) {
             try {
                 String videoPath = String.valueOf(extras.get("video"));
-
                 videoView = findViewById(R.id.videoView);
-
                 Uri uri = Uri.parse(videoPath);
 
+                MediaController mc = new MediaController(this);
+                videoView.setMediaController(mc);
                 videoView.setVideoURI(uri);
+                videoView.requestFocus();
+                videoView.start();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         }
-
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.start();
-            }
-        });
-    }
-
-    public void hideSystemUI() {
-//        https://medium.com/marojuns-android/android-immersive-mode-a4ae2065c0a2
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
